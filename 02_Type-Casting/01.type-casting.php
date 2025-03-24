4ab5cae770cf312600ac7eb7e7c6483dd98284a5
tomarcodinglife <tomarcodinglife@gmail.com>
2025-03-24 19:52:23 +0530

    updated

---
+++ 02_Type-Casting/01.type-casting.php
@@ -0,0 +1,32 @@
+<?php
+
+// int to string
+$val = 5;
+$stval = (string) $val;
+echo var_dump($stval);
+
+echo "</br>";
+
+//string to int
+$value = "300";
+$intval = (int) $value;
+echo var_dump($intval);
+
+echo "</br>";
+
+//int to flot
+$fvalu = 1;
+$fval = (float) $fvalu ;
+echo var_dump($fval)
+
+//string to object
+
+//string to array
+
+
+//
+
+
+
+
+?>
