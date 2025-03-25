b982f16315b4154863d87e3efa1f10794e29cc2f
tomarcodinglife <tomarcodinglife@gmail.com>
2025-03-25 20:19:56 +0530

    UPDATED

--- 06_Function/8.functionCall-with-button.php
+++ 06_Function/8.functionCall-with-button.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Call PHP Function with Button</title>
-</head>
-<body>
-    
-    <form action="" method="post">
 
-        <button name="button" value="btn">Function Call</button>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Call PHP Function with Button</title>
+    </head>
 
-    </form>
-</body>
+    <body>
+        <form action="" method="post">
+            <button name="button" value="btn">Function Call</button>
+        </form>
+    </body>
+
 </html>
 
 <?php
 
-if(isset($_REQUEST['button'])){
-    buttonFunction();
-}
+    if(isset($_REQUEST['button'])){
+        buttonFunction();
+    }
 
-
-function buttonFunction(){
-    echo "Hi this is button function";
-}
+    function buttonFunction(){
+        echo "Hi this is button function";
+    }
 
 ?>
