0dbda9a082cac11634fc6e26efed80d62aa85ed3
tomarcodinglife <tomarcodinglife@gmail.com>
2025-03-28 19:23:29 +0530

    updated

--- 10_file-handle/5.folder-file-list.php
+++ 10_file-handle/5.folder-file-list.php
@@ -1,20 +1,20 @@
 <?php
 
-$path = "C:/xampp/htdocs/php/10_file-handle";
+$path = "demo-file";
 $items = scandir($path);
 
-print_r($items); 
+// print_r($items); 
 // return -   Array ( [0] => . [1] => .. [2] => 1.file_create.php [3] => 2.file-with-html.php [4] => 3.file-read.php [5] => 4.file-read.php [6] => 5.folder-file-list.php [7] => abc [8] => demo [9] => dummy.txt [10] => sujit.txt ) 
 
 
 // for remove . and ..
 $newitems = array_diff($items, array(".", ".."));
-print_r($newitems); 
+// print_r($newitems); 
 // return - Array ( [2] => 1.file_create.php [3] => 2.file-with-html.php [4] => 3.file-read.php [5] => 4.file-read.php [6] => 5.folder-file-list.php [7] => abc [8] => demo [9] => dummy.txt [10] => sujit.txt ) 
 
 
 foreach ($items as $it) {
-    echo "<a href=$path/$it>$it<a>";
+    echo "<a href=$path/$it>$it</a>";
     echo "<br>";
 }
 
